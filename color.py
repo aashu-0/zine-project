31407bbc4cab6c55ca92c47c5db0656fe4076d65
aashu <157953935+aashu-0@users.noreply.github.com>
2024-02-05 18:56:05 +0530

    Update color.py

--- color.py
+++ color.py
@@ -4,16 +4,12 @@ import numpy as np
 def function(value):
     print(value)
 
-# Load image
 image = cv2.imread('Photos/WIN_20240201_22_56_29_Pro.jpg')
 
-# Create a window
 cv2.namedWindow('image')
 
 cv2.resizeWindow('image', 600, 400)
 
-# Create trackbars for color change
-# Hue is from 0-179 for Opencv
 cv2.createTrackbar('HMin', 'image', 0, 179, function)
 cv2.createTrackbar('SMin', 'image', 0, 255, function)
 cv2.createTrackbar('VMin', 'image', 0, 255, function)
@@ -21,17 +17,14 @@ cv2.createTrackbar('HMax', 'image', 0, 179, function)
 cv2.createTrackbar('SMax', 'image', 0, 255, function)
 cv2.createTrackbar('VMax', 'image', 0, 255, function)
 
-# Set default value for Max HSV trackbars
 cv2.setTrackbarPos('HMax', 'image', 179)
 cv2.setTrackbarPos('SMax', 'image', 255)
 cv2.setTrackbarPos('VMax', 'image', 255)
 
-# Initialize HSV min/max values
 hMin = sMin = vMin = hMax = sMax = vMax = 0
 phMin = psMin = pvMin = phMax = psMax = pvMax = 0
 
 while True:
-    # Get current positions of all trackbars
     hMin = cv2.getTrackbarPos('HMin', 'image')
     sMin = cv2.getTrackbarPos('SMin', 'image')
     vMin = cv2.getTrackbarPos('VMin', 'image')
@@ -39,16 +32,13 @@ while True:
     sMax = cv2.getTrackbarPos('SMax', 'image')
     vMax = cv2.getTrackbarPos('VMax', 'image')
 
-    # Set minimum and maximum HSV values to display
     lower = np.array([hMin, sMin, vMin])
     upper = np.array([hMax, sMax, vMax])
 
-    # Convert to HSV format and color threshold
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower, upper)
     result = cv2.bitwise_and(image, image, mask=mask)
 
-    # Print if there is a change in HSV value
     if((phMin != hMin) | (psMin != sMin) | (pvMin != vMin) | (phMax != hMax) | (psMax != sMax) | (pvMax != vMax) ):
         print("(hMin = %d , sMin = %d, vMin = %d), (hMax = %d , sMax = %d, vMax = %d)" % (hMin , sMin , vMin, hMax, sMax , vMax))
         phMin = hMin
@@ -58,7 +48,6 @@ while True:
         psMax = sMax
         pvMax = vMax
 
-    # Display result image
     cv2.imshow('image', result)
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
