099eb82a70a5fd8e6dc83ad124ed02986de646b9
aashutosh <aashutoshmishra548@gmail.com>
2024-02-05 19:00:33 +0530

    update

--- detection.py
+++ detection.py
@@ -1,23 +1,18 @@
 import numpy as np
 import cv2
 
-# Define object specific variables
 dist = 0
 focal = 450
 pixels = 30
 width = 6
 
-
-# find the distance from then camera
-def get_dist(rectange_params, image):
-    # find no of pixels covered
-    pixels = rectange_params[1][0]
+def get_dist(rectangle_params, image):
+    pixels = rectangle_params[1][0]
     print(pixels)
-    # calculate distance
+
     dist = (width * focal) / pixels
 
-    # Wrtie n the image
-    image = cv2.putText(image, 'Distance from Camera in CM :', org, font,
+    image = cv2.putText(image, 'Distance from Camera in cm :', org, font,
                         1, color, 2, cv2.LINE_AA)
 
     image = cv2.putText(image, str(dist), (110, 50), font,
@@ -26,10 +21,9 @@ def get_dist(rectange_params, image):
     return image
 
 
-# Extract Frames
+
 cap = cv2.VideoCapture(0)
 
-# basic constants for opencv Functs
 kernel = np.ones((3, 3), 'uint8')
 font = cv2.FONT_HERSHEY_SIMPLEX
 org = (0, 20)
@@ -37,31 +31,28 @@ fontScale = 0.6
 color = (0, 0, 255)
 thickness = 2
 
-cv2.namedWindow('Object Dist Measure ', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('Object Dist Measure ', 700, 600)
+cv2.namedWindow('Object Distance Measure ', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('Object Distance Measure ', 700, 600)
 
-# loop to capture video frames
 while True:
     ret, img = cap.read()
 
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    # predefined mask for object colour detection
+    # mask for detection
+    # change the ranges as per object using color.py program
     lower = np.array([21, 0, 194])
     upper = np.array([179, 255, 255])
     mask = cv2.inRange(hsv_img, lower, upper)
 
-    # Remove Extra garbage from image
+    # garbage removal
     d_img = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=5)
 
-    # find the histogram
     cont, hei = cv2.findContours(d_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cont = sorted(cont, key=cv2.contourArea, reverse=True)[:1]
 
     for cnt in cont:
-        # check for contour area
         if (cv2.contourArea(cnt) > 100 and cv2.contourArea(cnt) < 306000):
-            # Draw a rectange on the contour
             rect = cv2.minAreaRect(cnt)
             box = cv2.boxPoints(rect)
             box = np.int0(box)
