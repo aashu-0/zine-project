1596fb888f0413e7644e1163630bb00b070ec642
aashutosh <aashutoshmishra548@gmail.com>
2024-02-05 19:42:10 +0530

    distance measure commit

--- detection.py
+++ detection.py
@@ -1,10 +1,11 @@
-import numpy as np
 import cv2
+import numpy as np
 
 dist = 0
 focal = 450
 pixels = 30
 width = 6
+
 
 def get_dist(rectangle_params, image):
     pixels = rectangle_params[1][0]
@@ -21,8 +22,11 @@ def get_dist(rectangle_params, image):
     return image
 
 
+def empty(img):
+    pass
 
-cap = cv2.VideoCapture(0)
+
+video = cv2.VideoCapture(0)
 
 kernel = np.ones((3, 3), 'uint8')
 font = cv2.FONT_HERSHEY_SIMPLEX
@@ -31,21 +35,29 @@ fontScale = 0.6
 color = (0, 0, 255)
 thickness = 2
 
-cv2.namedWindow('Object Distance Measure ', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('Object Distance Measure ', 700, 600)
+cv2.namedWindow('TrackBar')
+cv2.resizeWindow('TrackBar', 600, 300)
+cv2.createTrackbar('hue_min', 'TrackBar', 0, 179, empty)
+cv2.createTrackbar('hue_max', 'TrackBar', 179, 179, empty)
+cv2.createTrackbar('sat_min', 'TrackBar', 0, 255, empty)
+cv2.createTrackbar('sat_max', 'TrackBar', 255, 255, empty)
+cv2.createTrackbar('val_min', 'TrackBar', 0, 255, empty)
+cv2.createTrackbar('val_max', 'TrackBar', 255, 255, empty)
 
 while True:
-    ret, img = cap.read()
+    ret, img = video.read()
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    hue_min = cv2.getTrackbarPos('hue_min', 'TrackBar')
+    hue_max = cv2.getTrackbarPos('hue_max', 'TrackBar')
+    sat_min = cv2.getTrackbarPos('sat_min', 'TrackBar')
+    sat_max = cv2.getTrackbarPos('sat_max', 'TrackBar')
+    val_min = cv2.getTrackbarPos('val_min', 'TrackBar')
+    val_max = cv2.getTrackbarPos('val_max', 'TrackBar')
 
-    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower = np.array([hue_min, sat_min, val_min])
+    upper = np.array([hue_max, sat_max, val_max])
+    mask = cv2.inRange(hsv, lower, upper)
 
-    # mask for detection
-    # change the ranges as per object using color.py program
-    lower = np.array([21, 0, 194])
-    upper = np.array([179, 255, 255])
-    mask = cv2.inRange(hsv_img, lower, upper)
-
-    # garbage removal
     d_img = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=5)
 
     cont, hei = cv2.findContours(d_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -59,8 +71,9 @@ while True:
             cv2.drawContours(img, [box], -1, (255, 0, 0), 3)
 
             img = get_dist(rect, img)
-
-    cv2.imshow('Object Dist Measure ', img)
+    cv2.imshow('Frame', img)
+    cv2.imshow('hsv', hsv)
+    cv2.imshow('mask', mask)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
