b37c6b28597f8f789a0d68d9f2badb6d699ae076
aashutosh <aashutoshmishra548@gmail.com>
2024-02-05 18:31:36 +0530

    commit

--- color-detect.py
+++ color-detect.py
@@ -13,12 +13,12 @@ while True:
     blue_lower = np.array([99, 115, 150], np.uint8)
     blue_upper = np.array([110, 255, 255], np.uint8)
 
-    yellow_lower = np.array([22, 60, 200], np.uint8)
-    yellow_upper = np.array([60, 255, 255], np.uint8)
+    green_lower = np.array([40, 40, 40], np.uint8)
+    green_upper = np.array([80, 255, 255], np.uint8)
 
     red = cv2.inRange(hsv, red_lower, red_upper)
     blue = cv2.inRange(hsv, blue_lower, blue_upper)
-    yellow = cv2.inRange(hsv, yellow_lower, yellow_upper)
+    green = cv2.inRange(hsv, green_lower, green_upper)
 
     kernal = np.ones((5, 5), "uint8")
 
@@ -28,11 +28,11 @@ while True:
     blue = cv2.dilate(blue, kernal)
     res1 = cv2.bitwise_and(img, img, mask=blue)
 
-    yellow = cv2.dilate(yellow, kernal)
-    res2 = cv2.bitwise_and(img, img, mask=yellow)
+    green = cv2.dilate(green, kernal)
+    res2 = cv2.bitwise_and(img, img, mask=green)
 
     (contours, hierarchy) = cv2.findContours(red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # Tracking the Red Color
+    # Red Color
     for pic, contour in enumerate(contours):
         area = cv2.contourArea(contour)
         if (area > 300):
@@ -40,9 +40,8 @@ while True:
             img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
             cv2.putText(img, "Red", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255))
 
-    # Tracking the Blue Color
+    # Blue Color
     (contours, hierarchy) = cv2.findContours(blue, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
     for pic, contour in enumerate(contours):
         area = cv2.contourArea(contour)
         if (area > 300):
@@ -50,16 +49,16 @@ while True:
             img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
             cv2.putText(img, "Blue", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0))
 
-    # Tracking the Yellow Color
-    (contours, hierarchy) = cv2.findContours(yellow, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # Green Color
+    (contours, hierarchy) = cv2.findContours(green, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for pic, contour in enumerate(contours):
         area = cv2.contourArea(contour)
         if (area > 300):
             x, y, w, h = cv2.boundingRect(contour)
             img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(img, "Yellow", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0))
+            cv2.putText(img, "Green", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0))
 
-    cv2.imshow("Color Tracking", img)
+    cv2.imshow("Color Detection", img)
     if cv2.waitKey(10) & 0xFF == ord('q'):
         cap.release()
         cv2.destroyAllWindows()
