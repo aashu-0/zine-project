a252c064121d7e5c19e6adf6e656c8bf87d937f0
aashu <157953935+aashu-0@users.noreply.github.com>
2024-02-10 09:09:03 +0530

    Update color.py

--- color.py
+++ color.py
@@ -1,21 +1,6 @@
 import cv2
 import numpy as np
 
-#def function(value):
-   # print(value)
-
-#image = cv2.imread('Photos/WIN_20240201_22_56_29_Pro.jpg')
-
-#cv2.namedWindow('image')
-#cv2.resizeWindow('image', 600, 300)
-
-#cv2.createTrackbar('HMin', 'image', 0, 179, function)
-#cv2.createTrackbar('SMin', 'image', 0, 255, function)
-#cv2.createTrackbar('VMin', 'image', 0, 255, function)
-#cv2.createTrackbar('HMax', 'image', 0, 179, function)
-#cv2.createTrackbar('SMax', 'image', 0, 255, function)
-#cv2.createTrackbar('VMax', 'image', 0, 255, function)
-
 def empty(img):
     pass
 
@@ -37,19 +22,6 @@ cv2.createTrackbar('val_max', 'TrackBar', 255, 255, empty)
 hMin = sMin = vMin = hMax = sMax = vMax = 0
 phMin = psMin = pvMin = phMax = psMax = pvMax = 0
 
-#while True:
-    #hMin = cv2.getTrackbarPos('HMin', 'image')
-    #sMin = cv2.getTrackbarPos('SMin', 'image')
-    #vMin = cv2.getTrackbarPos('VMin', 'image')
-    #hMax = cv2.getTrackbarPos('HMax', 'image')
-    #sMax = cv2.getTrackbarPos('SMax', 'image')
-    #vMax = cv2.getTrackbarPos('VMax', 'image')
-
-    #lower = np.array([hMin, sMin, vMin])
-    #upper = np.array([hMax, sMax, vMax])
-
-    #hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    #mask = cv2.inRange(hsv, lower, upper)
 
 while True:
     ret, img = video.read()
